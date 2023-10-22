132c7120e77569a009cc01ffbd1d409744132080
arveymenon <arveymenon@gmail.com>
2023-10-22 11:35:46 +0530

    neon update

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,28 @@ const navigation = [
 	{ name: "Contact", href: "/contact" },
 ];
 
+const neonText = {
+	"color": "#fff", 
+	"text-shadow": "0 0 0px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 0px #860eff, 0 0 2px #0fa, 0 0 20px #860eff, 0 0 102px #0fa, 0 0 151px #340068"
+}
+
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
 			<nav className="my-16 animate-fade-in">
 				<ul className="flex flex-wrap items-center justify-center gap-4">
-					{navigation.map((item) => (
-						<Link
-							key={item.href}
-							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-						>
-							{item.name}
-						</Link>
+					{navigation.map((item, index) => (
+						<>
+							<Link
+								key={item.href}
+								href={item.href}
+								className="text-2xl duration-500 text-zinc-500 hover:text-zinc-300"
+							>
+								{item.name}
+							</Link>
+							{ index == 0 && <span className="text-4xl duration-500 text-zinc-500 hover:text-zinc-300">|</span> }
+						</>
+
 					))}
 				</ul>
 			</nav>
@@ -28,33 +37,21 @@ export default function Home() {
 				className="absolute inset-0 -z-10 animate-fade-in"
 				quantity={100}
 			/>
-			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text neonText-1">
+				Arulvinayak
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 mx-6">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
+					Hi, my name is Arulvinayak Menon, I'm building amazing web3 application and open source solutions. Checkout my {" "}
 					<Link
 						target="_blank"
-						href="https://upstash.com"
+						href="https://github.com/Arveymenon"
 						className="underline duration-500 hover:text-zinc-300"
-					>
-						Upstash
+					> 
+						Github 
 					</Link>
-
-					
-					<wbr /> and working on{" "}
-					<Link
-						target="_blank"
-						href="https://unkey.dev"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						unkey.dev
-					</Link>{" "}
-					at night.
 				</h2>
 			</div>
 		</div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -42,7 +42,7 @@ export default async function ProjectsPage() {
 			<Navigation />
 			<div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
 				<div className="max-w-2xl mx-auto lg:mx-0">
-					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl neonText-purple">
 						Projects
 					</h2>
 					<p className="mt-4 text-zinc-400">
@@ -77,7 +77,7 @@ export default async function ProjectsPage() {
 
 								<h2
 									id="featured-post"
-									className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+									className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display neonText-green"
 								>
 									{featured.title}
 								</h2>
@@ -96,20 +96,19 @@ export default async function ProjectsPage() {
 					<div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
 						{[top2, top3].map((project) => (
 							<Card key={project.slug}>
-								<Article project={project} views={views[project.slug] ?? 0} />
+								<Article project={project} titleClasses="neonText-red" views={views[project.slug] ?? 0} />
 							</Card>
 						))}
 					</div>
 				</div>
 				<div className="hidden w-full h-px md:block bg-zinc-800" />
-
 				<div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
 					<div className="grid grid-cols-1 gap-4">
 						{sorted
 							.filter((_, i) => i % 3 === 0)
 							.map((project) => (
 								<Card key={project.slug}>
-									<Article project={project} views={views[project.slug] ?? 0} />
+									<Article project={project} titleClasses="neonText-blue" views={views[project.slug] ?? 0} />
 								</Card>
 							))}
 					</div>
@@ -118,7 +117,7 @@ export default async function ProjectsPage() {
 							.filter((_, i) => i % 3 === 1)
 							.map((project) => (
 								<Card key={project.slug}>
-									<Article project={project} views={views[project.slug] ?? 0} />
+									<Article project={project} titleClasses="neonText-blue" views={views[project.slug] ?? 0} />
 								</Card>
 							))}
 					</div>
@@ -127,7 +126,7 @@ export default async function ProjectsPage() {
 							.filter((_, i) => i % 3 === 2)
 							.map((project) => (
 								<Card key={project.slug}>
-									<Article project={project} views={views[project.slug] ?? 0} />
+									<Article project={project} titleClasses="neonText-blue" views={views[project.slug] ?? 0} />
 								</Card>
 							))}
 					</div>
